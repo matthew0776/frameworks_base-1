SlimRoms/frameworks_base

Frameworks: Slim heads up customizations for LP (1/2)
Basically it follows the same idea like we had on SlimKat....see here
https://github.com/SlimRoms/frameworks_base/commit/323ed77c5e5bd6d421beae1f8118eb580be5ecf1

That said there are some differences in this more or less completely recoded feature.

- Moved it into the app policy files
- Enable/Disable on every app (no system apps)  either longpress a notification and an button appears / or in notification app settings
- For now only snooze and heads up timeout configurable....other stuff will follow later.
- fix up some AOSP code for it as well fix some UX inconsitence.
- left swipe dismiss, right swipe put into background, swipe down opens notification drawer and moves the notification into it
  (aosp default behaviour is a big UX disaster....swipe up dismisses it completely and swipe down open notification drawer but heads up stays....first
   happened to me 10000 times accidentally which is really bad and second does not make sense. Hence this change of the default behaviour)

Well and a lot smaller things as usual.

Last PS
- fixed some minor stuff
- reset snooze timer when screen goes off
- do not show headups if notification drawer is visible

PS
making snooze button functional - we may need to move it a bit down.

PS
give snooze button a bit animation love

PS
icons

PS
add accessibility descriptions etc

PS
hedasupcandidate package fix
move snooze timout into config
smaller fixes
out off WIP and ready to merge
added commit description

Still ToDo:
Settings

Change-Id: Ic8977feb4bb0700f1093ce7d62c22d4c63c69bad

packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -59,6 +59,8 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_NOTIFICATION_LIGHT_PULSE           = 17 << MSG_SHIFT;
     private static final int MSG_SET_AUTOROTATE_STATUS              = 18 << MSG_SHIFT;
     private static final int MSG_START_CUSTOM_INTENT_AFTER_KEYGUARD = 19 << MSG_SHIFT;
+    private static final int MSG_HIDE_HEADS_UP_CANDIDATE            = 20 << MSG_SHIFT;
+    private static final int MSG_HIDE_HEADS_UP                      = 21 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -102,6 +104,8 @@ public class CommandQueue extends IStatusBar.Stub {
         public void notificationLightPulse(int argb, int onMillis, int offMillis);
         public void setAutoRotate(boolean enabled);
         public void showCustomIntentAfterKeyguard(Intent intent);
+        public void hideHeadsUpCandidate(String packageName);
+        public void scheduleHeadsUpClose();
     }
 
     public CommandQueue(Callbacks callbacks, StatusBarIconList list) {
@@ -257,6 +261,21 @@ public class CommandQueue extends IStatusBar.Stub {
         m.sendToTarget();
     }
 
+    public void hideHeadsUpCandidate(String packageName) {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_HIDE_HEADS_UP_CANDIDATE);
+            mHandler.obtainMessage(MSG_HIDE_HEADS_UP_CANDIDATE,
+                0, 0, packageName).sendToTarget();
+        }
+    }
+
+    public void scheduleHeadsUpClose() {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_HIDE_HEADS_UP);
+            mHandler.sendEmptyMessage(MSG_HIDE_HEADS_UP);
+        }
+    }
+
     private final class H extends Handler {
         public void handleMessage(Message msg) {
             final int what = msg.what & MSG_MASK;
@@ -341,6 +360,12 @@ public class CommandQueue extends IStatusBar.Stub {
                     break;
                 case MSG_START_CUSTOM_INTENT_AFTER_KEYGUARD:
                     mCallbacks.showCustomIntentAfterKeyguard((Intent) msg.obj);
+                    break;
+                case MSG_HIDE_HEADS_UP_CANDIDATE:
+                    mCallbacks.hideHeadsUpCandidate((String) msg.obj);
+                    break;
+                case MSG_HIDE_HEADS_UP:
+                    mCallbacks.scheduleHeadsUpClose();
                     break;
             }
         }

matthew0776/frameworks_base-1

Conflicts:
	core/java/android/app/NotificationGroup.aidl
	core/res/res/values/slim_strings.xml
	packages/SystemUI/AndroidManifest.xml
	packages/SystemUI/res/layout/notification_guts.xml
	packages/SystemUI/res/values/config.xml
	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
	services/core/java/com/android/server/am/ActivityStackSupervisor.java
	core/java/android/provider/Settings.java
	core/java/com/android/internal/statusbar/IStatusBar.aidl
	core/java/com/android/internal/statusbar/IStatusBarService.aidl
	packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
	packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
	services/core/java/com/android/server/statusbar/StatusBarManagerService.java

packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -38,7 +38,6 @@ public class CommandQueue extends IStatusBar.Stub {
 
     private static final int OP_SET_ICON    = 1;
     private static final int OP_REMOVE_ICON = 2;
-
     private static final int MSG_ICON                       = 1 << MSG_SHIFT;
     private static final int MSG_DISABLE                    = 2 << MSG_SHIFT;
     private static final int MSG_EXPAND_NOTIFICATIONS       = 3 << MSG_SHIFT;
@@ -57,6 +56,9 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_NOTIFICATION_LIGHT_OFF     = 16 << MSG_SHIFT;
     private static final int MSG_NOTIFICATION_LIGHT_PULSE   = 17 << MSG_SHIFT;
     private static final int MSG_ANIMATE_PANEL_FROM_NAVBAR = 18 << MSG_SHIFT;
+    private static final int MSG_HIDE_HEADS_UP_CANDIDATE    = 19 << MSG_SHIFT;
+    private static final int MSG_HIDE_HEADS_UP              = 20 << MSG_SHIFT;
+
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -100,6 +102,8 @@ public class CommandQueue extends IStatusBar.Stub {
         public void notificationLightOff();
         public void notificationLightPulse(int argb, int onMillis, int offMillis);
         public void notifyLayoutChange(int direction);
+        public void hideHeadsUpCandidate(String packageName);
+        public void scheduleHeadsUpClose();
     }
 
     public CommandQueue(Callbacks callbacks, StatusBarIconList list) {
@@ -252,6 +256,21 @@ public class CommandQueue extends IStatusBar.Stub {
         mCallbacks.notifyLayoutChange(direction);
     }
 
+    public void hideHeadsUpCandidate(String packageName) {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_HIDE_HEADS_UP_CANDIDATE);
+            mHandler.obtainMessage(MSG_HIDE_HEADS_UP_CANDIDATE,
+                0, 0, packageName).sendToTarget();
+        }
+    }
+
+    public void scheduleHeadsUpClose() {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_HIDE_HEADS_UP);
+            mHandler.sendEmptyMessage(MSG_HIDE_HEADS_UP);
+        }
+    }
+
     private final class H extends Handler {
         public void handleMessage(Message msg) {
             final int what = msg.what & MSG_MASK;
@@ -333,6 +352,12 @@ public class CommandQueue extends IStatusBar.Stub {
                     break;
                 case MSG_NOTIFICATION_LIGHT_PULSE:
                     mCallbacks.notificationLightPulse((Integer) msg.obj, msg.arg1, msg.arg2);
+                    break;
+                case MSG_HIDE_HEADS_UP_CANDIDATE:
+                    mCallbacks.hideHeadsUpCandidate((String) msg.obj);
+                    break;
+                case MSG_HIDE_HEADS_UP:
+                    mCallbacks.scheduleHeadsUpClose();
                     break;
             }
         }